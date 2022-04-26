0df83ad17b58e228d68dd6acf2d7cfc6cc608ac7
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-27 01:21:43 +0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import cors, { CorsOptions } from "cors";
 import genericErrorHandler from "./middlewares/errorHandler";
 import usersRouter from "./users";
 
 const app = express();
 
+const whitelist = ['http://localhost:3000', 'http://example2.com']
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
 app.use(express.json())
+app.use(cors(corsOptions));
 
 const apiRouter = express.Router();
 
