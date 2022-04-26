2d8113752c27c193c199fd15979d6ac71037a7dc
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-27 01:21:33 +0400

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,7 @@
 interface User {
     _id: string
     nick: string
-    pubkey: string
+    publicKey: string
     password?: string
 }
 
@@ -13,4 +13,33 @@ namespace Express {
     interface Request {
         user?: string
     }
+}
+
+// Shared with Frontend:
+
+interface LoginResponse {
+    token: string
+    user: User
+}
+
+interface User {
+    _id: string
+    username: string
+    publicKey: string
+}
+
+interface Message {
+    sender: User["_id"]
+    to: User["_id"][]
+    chatId: string // calc as sha256([sender, ...to].sort().join())
+    content: {
+        text: string;
+        media?: string
+    }
+    timestamp: number
+}
+
+interface Chat {
+    messages: Message[];
+    members: User[]
 }
