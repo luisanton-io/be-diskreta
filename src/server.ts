3bcce562cdd0685e7b86113bdd4511ab2076ada1
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-27 04:06:38 +0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -15,23 +15,50 @@ io.on("connection", socket => {
     try {
         const { _id } = jwt.verify(socket.handshake.auth.token, process.env.JWT_SECRET!) as JWTPayload
 
-        shared.onlineUsers.push({ _id, socket })
+        const onlineUser = shared.onlineUsers.find(u => u._id === _id)
 
-        socket.emit('dequeue', shared.messageQueue.filter(m => m.to.includes(_id)))
-        shared.messageQueue = shared.messageQueue.filter(m => !m.to.includes(_id))
+        if (onlineUser) {
+            onlineUser.sockets.push(socket)
+        } else {
+            shared.onlineUsers.push({ _id, sockets: [socket] })
+        }
+
+        console.table({ _id })
+
+        socket.emit('dequeue', shared.messageQueue.filter(m => m.to.some(recipient => recipient._id === _id)))
+        shared.messageQueue = shared.messageQueue.filter(m => m.to.some(recipient => recipient._id !== _id))
 
 
         socket.on("out-msg", (msg: Message) => {
-            for (const userId of msg.to) {
-                const user = shared.onlineUsers.find(u => u._id === userId)
+            console.table({ 'Received message': msg.content.text })
 
-                if (user) user.socket.emit("in-msg", msg)
+            for (const recipient of msg.to) {
+                const user = shared.onlineUsers.find(u => u._id === recipient._id)
+
+                if (user) {
+                    for (const socket of user.sockets) {
+                        socket.emit("in-msg", msg)
+                    }
+                }
                 else shared.messageQueue.push(msg)
             }
         })
 
         socket.on('disconnect', () => {
-            shared.onlineUsers = shared.onlineUsers.filter(user => user.socket.id !== socket.id)
+            let socketIx = 0
+            const onlineUserIx =
+                shared.onlineUsers.findIndex(u => {
+                    socketIx = u.sockets.findIndex(s => s.id === socket.id)
+                    return socketIx !== -1
+                })
+
+            const onlineUser = shared.onlineUsers[onlineUserIx]
+
+            if (onlineUser) {
+                onlineUser.sockets.splice(socketIx, 1)
+                !onlineUser.sockets.length && shared.onlineUsers.splice(onlineUserIx, 1)
+            }
+
         })
 
     } catch (error) {
