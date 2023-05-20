3446dbecf1edb860a4b7709cb5cae8acc46f682d
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-05-20 19:36:00 +0400

    No comments

--- src/events/messageReaction.ts
+++ src/events/messageReaction.ts
@@ -3,7 +3,6 @@ import User from "../users/model"
 
 const messageReaction = async ({ chatId, hash, senderId, recipientId, reaction }: ReactionPayload) => {
     // console.log("reaction", reaction)
-    console.table({ chatId, hash, senderId, recipientId, reaction })
     const incomingReaction = { chatId, hash, senderId, reaction }
     // send reaction to recipient
     const recipientSocket = shared.onlineUsers[recipientId]?.socket
@@ -13,15 +12,13 @@ const messageReaction = async ({ chatId, hash, senderId, recipientId, reaction }
                 resolve(false)
             }, 3000)
 
-            console.log("emitting reaction to", recipientId)
-
             recipientSocket.emit("in-reaction", incomingReaction, () => {
                 clearTimeout(timeout)
                 console.log("Emitted reaction to", recipientId)
                 resolve(true)
             })
         }))) {
-        console.log("No connection to", recipientId, "saving to queue")
+
         await User.updateOne({
             _id: recipientId,
         }, {
